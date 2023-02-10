3dde599705c121ba7597d3c827324e0eabc56093
Sidharth Shyamsukha <39914259+sidharth20@users.noreply.github.com>
2023-02-10 13:27:45 -0500

    Time: 130 ms (46.71%), Space: 68.4 MB (94.03%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int maxright, l=arr.size();
-        maxright=arr[l-1];
-        arr[l-1] = -1;
+//         int maxright, l=arr.size();
+//         maxright=arr[l-1];
+//         arr[l-1] = -1;
         
-        if(l==1) return arr;
+//         if(l==1) return arr;
         
-        for(int i=l-2;i>=0;i--){
-            int tmp;
-            tmp=arr[i];
+//         for(int i=l-2;i>=0;i--){
+//             int tmp;
+//             tmp=arr[i];
+//             arr[i]=maxright;
+//             maxright=max(maxright,tmp);
+//             // arr[i]=maxright;
+//             // maxright=max(maxright,arr[i]);
+//         }
+//         return arr;
+        
+        int maxright=-1;
+        for(int i=arr.size()-1;i>=0;i--){
+            int tmp=arr[i];
             arr[i]=maxright;
             maxright=max(maxright,tmp);
-            // arr[i]=maxright;
-            // maxright=max(maxright,arr[i]);
         }
         return arr;
     }
