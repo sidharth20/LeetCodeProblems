6cf7ce169f38bcc1fedbe2b9ffe4b334fe8c1c6a
Sidharth Shyamsukha <39914259+sidharth20@users.noreply.github.com>
2023-02-22 10:33:14 -0500

    Time: 223 ms (50.84%), Space: 102.8 MB (60.01%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
      int n = temperatures.size();
-        stack<int> st;// stack stores the indices of temperatures
+        stack<int> st;// stack stores the indices of temperatures in decreasing order
+        // it's a monotonic decreasing stack - all values in decreasing order
         vector<int> ans(n,0);
         for(int i=0;i<n;i++){
             while(!st.empty() and temperatures[st.top()]<temperatures[i]){
