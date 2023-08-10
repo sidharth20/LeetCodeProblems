55f5ee48a74b67fe49bc6ae9faf62e0243f94750
Sidharth Shyamsukha <39914259+sidharth20@users.noreply.github.com>
2023-08-10 15:39:29 -0400

    Time: 10 ms (85.29%), Space: 10 MB (79.68%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
+        int rows=board.size();
+        int col=board[0].size();
         
-        //identifying all the O's at the edges and running dfs from there
-        for(int i=0;i<m;i++){
-            if(board[i][0]=='O') dfs(board,i,0,m,n);
+        //flipping zeroes on the edges
+        for(int i=0;i<rows;i++){
+            if(board[i][0]=='O') dfs(board,rows,col,i,0);
             
-            if(board[i][n-1]=='O') dfs(board,i,n-1,m,n);
+            if(board[i][col-1]=='O') dfs(board,rows,col,i,col-1);
         }
         
-        for(int j=0;j<n;j++){
-            if(board[0][j]=='O') dfs(board,0,j,m,n);
+        for(int j=0;j<col;j++){
+            if(board[0][j]=='O') dfs(board,rows,col,0,j);
             
-            if(board[m-1][j]=='O') dfs(board,m-1,j,m,n);
+            if(board[rows-1][j]=='O') dfs(board,rows,col,rows-1,j);
         }
         
-        //now flipping all the O's that are not connected to the edge
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<col;j++){
                 if(board[i][j]=='O') board[i][j]='X';
                 
                 if(board[i][j]=='#') board[i][j]='O';
+                
             }
         }
-        
     }
     
-    void dfs(vector<vector<char>>& board,int i,int j,int m,int n){
-        if(i<0 || i>=m || j<0 || j>=n || board[i][j]!='O') return;
+    void dfs(vector<vector<char>>& board, int rows, int col, int i, int j){
+        if(i<0 || i>=rows || j<0 || j>=col || board[i][j]!='O') return;
         
-        board[i][j]='#';
+        if( board[i][j]=='O') board[i][j]='#';
         
-        dfs(board,i-1,j,m,n);
-        dfs(board,i+1,j,m,n);
-        dfs(board,i,j-1,m,n);
-        dfs(board,i,j+1,m,n);
+        dfs(board,rows,col,i+1,j);
+        dfs(board,rows,col,i-1,j);
+        dfs(board,rows,col,i,j+1);
+        dfs(board,rows,col,i,j-1);
     }
 };
