ed4a3ed6557d20c8a03319fcb7273f832b267239
Sidharth Shyamsukha <39914259+sidharth20@users.noreply.github.com>
2023-10-24 00:54:29 -0400

    Time: 17 ms (26.36%), Space: 17 MB (13.09%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
+//     int deleteAndEarn(vector<int>& nums) {
+//         map<int,int> mp;
+//         int maxnum=0;
+        
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size();i++){
+//             mp[nums[i]]++;
+//         }
+//         int ans=0;
+//         int l = nums.size();
+        
+//         cout<<nums[l-1];
+//         vector<int> dp(nums[l-1]+1,0);
+//         if(mp[1]) {
+//             dp[1]=mp[1];
+//             ans=max(ans,dp[1]);
+//         }
+//         for(int i=2;i<dp.size();i++){
+//             if(mp[i]){
+//                 dp[i]=max(dp[i-1],dp[i-2]+mp[i]*i);
+//                 ans = max(ans,dp[i]);
+//             } 
+//             else dp[i]=dp[i-1];
+//         }
+//         return ans;
+//     }
+    
     int deleteAndEarn(vector<int>& nums) {
-        map<int,int> mp;
-        int maxnum=0;
-        
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        int ans=0;
-        int l = nums.size();
-        
-        cout<<nums[l-1];
-        vector<int> dp(nums[l-1]+1,0);
-        if(mp[1]) {
-            dp[1]=mp[1];
-            ans=max(ans,dp[1]);
-        }
-        for(int i=2;i<dp.size();i++){
-            if(mp[i]){
-                dp[i]=max(dp[i-1],dp[i-2]+mp[i]*i);
-                ans = max(ans,dp[i]);
-            } 
-            else dp[i]=dp[i-1];
-        }
-        return ans;
+    int n = 10001;
+    
+	//take the total sum by each number
+    vector<int> sum(n, 0);
+    vector<int> dp(n, 0);
+    
+    for(auto num: nums){
+        sum[num] += num;
     }
+    
+    dp[0] = 0;
+    dp[1] = sum[1];
+    //now apply the house robbing concept
+    for(int i=2; i<n; i++){
+        dp[i] = max(dp[i-2] + sum[i], dp[i-1]);
+    }
+    
+    return dp[n-1];
+}
 };
